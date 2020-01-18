8009a929163058a9b58d487b5576508e1cbda7f7
adonismk <59443588+adonismk@users.noreply.github.com>
2020-01-18 11:24:26 +0100

    Add files via upload

--- Triangle.py
+++ Triangle.py
@@ -1,82 +1,60 @@
-from math import*
-from sys import*
-# MWALE KALAISHA Adonis 
-# Master 1
-# Réseaux et Mobilité
-#Travail Pratique d'Algorithmique Avancée sur les triangles
 
+class Triangle:
+    def __init__(self,_a,_b,_c):
+        self.a=_a
+        self.b=_b
+        self.c=_c
+        self.cote=self.distance()
+        self.surface=0
+        self.perimetre=0
+    def is_align(self):
+        return self.a[0]==self.b[0] and self.b[0]==self.c[0]
 
-def Menu():
-    
-    print("**************************************************")
-    print("                                                  ")
-    print("*** 1. *** Saisir les informations du triangle ***")
-    print("*** 2. *** Afficher les informations du triangle *")
-    print("*** 3. ************ Type du triangle *************")
-    print("*** 4. ************** La superficie **************")
-    print("*** 5. ******** Trier selon la superficie ********")
-    print("*** 6. ************** QUITER *********************")
-    print("                                                  ")
-    print("**************************************************")
-    print("**************************************************")
-    print("                                                  ")
-    choix=int(input("Choisissez une option SVP!!!  "))
+# Nous avons créer la fonction distance pour calculer la longueur du segment entre deux points    
+    def distance(self):
+        return [(((self.a[0]-self.b[0])**2)+((self.a[1]-self.b[1])**2))**(0.5),(((self.c[0]-self.b[0])**2)+((self.c[1]-self.b[1])**2))**(0.5),(((self.a[0]-self.c[0])**2)+((self.a[1]-self.c[1])**2))**(0.5)]
+    def typeTriangle(self):
+        if self.is_align():
+            return "C'EST UN TRIANGLE PLAT"
+        elif self.cote[0]==self.cote[1] and self.cote[2]==self.cote[1]:
+            return "C'EST UN TRIANGLE EQUILATERAL" 
+        elif self.cote[0]==self.cote[1] or self.cote[1]==self.cote[0] or self.cote[2]==self.cote[0]:
+            return "C'EST UN TRIANGLE ISOCELE"
 
-
-    return choix
-
-if __name__ == "__main__":
-    k=Menu()
-    print("Donner les informations d'un triangle")
-
-d=1
-while d==1:
-    a=(input("Saisir la première valeur  : ")) #l'utilisateur saisi la première valeur d'un coté du triangle
-
- 
-    if a=="":
-        d=2
-
-    elif a==a:
-        b=(input("Saisir la deuxième valeur  : "))#l'utilisateur saisi la deuxième valeur d'un coté du triangle
-        c=(input("Saisir la troisième valeur : "))#l'utilisateur saisi la troisième valeur d'un coté du triangle
-        a,b,c=float(a),float(b),float(c)
-        
-    
-        #formule pour un triangle rectangle
-        if (a*a==b*b+c*c or b*b==a*a+c*c or c*c==a*a+b*b): 
-            print("**************************************************")        
-            print("C'est un triangle rectangle")
-            print("**************************************************")
-
-        #pour le cas où ce n'est pas un triangle
-        elif ((a+b<=c) or ((a+c<=b) or (b+c<=a))):
-            print("**************************************************")                 
-            print("Ce N'est PAS un triangle")
-            print("**************************************************")
-
-        #Cas pour un triangle rectangle équilatéral
-        elif (a==b or b==c or a==c):                               
-            if a==b==c:
-                print("**************************************************")
-                print("C'est un triangle équilatéral") 
-                print("**************************************************")
-
-            #Cas pour un triangle rectangle isocèle
-            elif (a*a==b*b+c*c or b*b==a*a+c*c or c*c==a*a+b*b):
-                print("**************************************************")
-                print("c'est un triangle rectangle isocèle")
-                print("**************************************************")
-                #Cas pour un triangle isocèle
-            else:
-                print("**************************************************")
-                print("C'est un triangle isocèle")
-                print("**************************************************")
-        
-        #Cas pour un triangle rectangle Scalène
-        else:
-            print("**************************************************")
-            print("C'est un triangle Scalène")
             
+          
 
-exit()
+a=[0,0]
+b=[0,0]
+c=[0,0]
+
+print("=================================================")
+print("### 1. Saisir les informations d'un triangle ====")
+print("### 2. Afficher les informations d'un triangle ==")
+print("### 3. Type du triangle =========================")
+print("### 4. La superficie ============================")
+print("### 5. Trier selon la superficie ================")
+print("### 6. QUITER ===================================")
+print("=================================================")
+
+menu=input("Choisissez une option SVP!!! \n")
+if menu == "1":
+    print("=================================================")
+    print("===== Saisir les informations d'un triangle =====")
+    print("=================================================")
+    print("INSERER LA VALEUR DE X1 :")
+    a[0]=int(input())
+    print("INSERER LA VALEUR DE Y1 :")
+    a[1]=int(input())
+    print("INSERER LA VALEUR DE X2 :")
+    b[0]=int(input())
+    print("INSERER LA VALEUR DE Y2 :")
+    b[1]=int(input())
+    print("INSERER LA VALEUR DE X3 :")
+    c[0]=int(input())
+    print("INSERER LA VALEUR DE Y3 :")
+    c[1]=int(input())
+    struct_triangle=Triangle(a,b,c)
+    print(struct_triangle.cote[0])
+    print(struct_triangle.cote[1])
+    print(struct_triangle.cote[2])
